79467fcb23f1b41b791a23627b3546740f7e356a
muna8928 <garnememuna@gmail.com>
2024-02-23 02:49:55 -0800

    added the basic search engine

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit", guideSearchSubmit);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);

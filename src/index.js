4d7798306859d1f348270ba13d407ed553989c2d
muna8928 <garnememuna@gmail.com>
2024-02-24 12:05:59 -0800

    finished the search engine and its functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searhCity(city) {
   let apiKey = "333f383e0cbe5983a0300b78e5t4eo0c";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}=${city}&key={apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-  searchCity(searchInput.value);
+
+  searhCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searhCity("Paris");

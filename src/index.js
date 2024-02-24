6d872b9342316e8f70541deca026e3719eb4c17b
muna8928 <garnememuna@gmail.com>
2024-02-24 09:15:17 -0800

    added the api key and the api url

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searhCity(city) {
+  let apiKey = "333f383e0cbe5983a0300b78e5t4eo0c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}=${city}&key={apiKey}&units=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");

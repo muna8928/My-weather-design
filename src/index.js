bc4a4a84b3cd0a65298188c7c13c0798c8dcef66
muna8928 <garnememuna@gmail.com>
2024-02-25 07:03:09 -0800

    included the weather icons to the app and moved the image src into the JavaScript

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   console.log(response.data);
+  iconElement.innerHTML = `<img
+              src="${response.data.condition.icon_url}"
+               / class="weather-app-icon">`;
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
